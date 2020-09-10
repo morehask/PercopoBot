054698f4a0a47c16a224829964ee9b7974323361
morehask <71052971+morehask@users.noreply.github.com>
2020-09-09 20:27:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const client = new Discord.Client()
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
 	var generalChannel = client.channels.cache.get("749587665606672426")
+	var me = client.channels.cache.get("729178175510282253")
 	var d = new Date();
 	if(d.getHours()==6){
 	    generalChannel.send("@everyone Good morning! Please remember to take your temp before leaving your room this morning (https://prodwebxe-hv.rose-hulman.edu/hsst-cgi/tool.pl). Also, don't forget your face mask!")
